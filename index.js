32f8bd96dfa2262de561b5eb947c55df26ae3775
George <ivanovgeorgi165@gmail.com>
2022-07-25 11:45:07 +0300

    Now fill with projects!

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-//template_jgt4zxr
-//service_3a0k4rl
-//h1-VFtZsoIiGTKA0k
 
 function contact(event){
-    // event.preventDefault();
-    // const loading = document.querySelector('.modal__overlay--loading')
-    // const success = document.querySelector('.modal__overlay--success')
-    // loading.classList += (" modal__overlay--visible");
-    // emailjs
-    // .sendForm(
-    //     'service_w2l71ih',
-    //     'template_jgt4zxr',
-    //     event.target,
-    //     'h1-VFtZsoIiGTKA0k'
-    // ).then(() => {
-    //     loading.classList.remove('modal__overlay--visible')
-    //     success.classList += (' modal__overlay--visible');
-    // }).catch(() => {
-    //     loading.classList.remove('modal__overlay--visible')
-    //     alert("Something went wrong.Please contact me on ivanovgeorgi165@gmail.com")
-    // })
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += (" modal__overlay--visible");
+    emailjs
+    .sendForm(
+        'service_w2l71ih',
+        'template_jgt4zxr',
+        event.target,
+        'h1-VFtZsoIiGTKA0k'
+    ).then(() => {
+        loading.classList.remove('modal__overlay--visible')
+        success.classList += (' modal__overlay--visible');
+    }).catch(() => {
+        loading.classList.remove('modal__overlay--visible')
+        alert("Something went wrong.Please contact me on ivanovgeorgi165@gmail.com")
+    })
    
 }
 let modalOpen=false
@@ -31,9 +28,28 @@ function openMenu(){
    
     modalOpen=true
      document.body.classList += ' modal--open';
-  
-    
-  
 }
+let background = false;
+function colorContrast(){
+     background = !background;
 
+     if(background){
+         document.body.classList += " dark-theme";
+     }
 
+    else {
+        document.body.classList.remove("dark-theme");
+    }
+}
+const scaleFactor= 1/ 20;
+function shapesFunction(event){
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for(let i = 0;i < shapes.length;i++){
+        const isOdd = i % 2 !== 0;
+        const boolOdd = isOdd ? -1: 1;
+        shapes[i].style.transform = `translate( ${ x * boolOdd}px, ${ y * boolOdd}px )`;
+    }
+}
